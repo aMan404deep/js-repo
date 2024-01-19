d1de15921b8cf88acf2291f1d923ab56a189b6b7
Amandeep Singh <amandeep101003@gmail.com>
2024-01-19 18:47:25 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+// let score = "33abc"
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); //number
+// console.log(valueInNumber); //NaN
+
+// let isLoggedIn = 1; //negative numbers also give true
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//empty string ,undefined , null all are false in boolean
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(stringNumber);
