df1dab72274189c71d57e2afea0dbcf7cf78beef
Amandeep Singh <amandeep101003@gmail.com>
2024-01-19 15:09:12 +0000

    new file added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "amandeep101@gmail.com"
+var accountPassword = "12345" // var is not used much (DON'T USE IT)
+accountCity = "Panipat"
+
+let accountState; //undefined
+
+// accountId = 2 //NOT ALLOWED 
+
+accountEmail = "as@gmail.com"
+accountPassword = "23124"
+accountCity = "Rajpura"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]); //to show all the variables in a table
+
