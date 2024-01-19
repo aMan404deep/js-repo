a31b2247c97d72ecd249bf2d385c12c2371973ec
Amandeep Singh <amandeep101003@gmail.com>
2024-01-19 18:20:13 +0000

    new file created

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+console.log("Amandeep")
+let name = "Amandeep"
+let age = 21
+let isLoggedIn = true
+let state;
+
+//number => 2 to the power of 53
+//bigint 
+//string => ""
+// boolean => true / false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "Amandeep")
+console.log(typeof null) //null is an object
+console.log(typeof undefined) //undefined is undefined type
