e9a6726c8a1b2c1e0215f0e2d47995479f4cca34
Amandeep Singh <amandeep101003@gmail.com>
2024-01-22 08:02:42 +0000

    learnt more about string

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -5,7 +5,7 @@ const repoCount = 50
 
 // console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('Amandeephc') //string is an object (not array)
+const gameName = new String('Amandeep-as') //string is an object (not array)
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -15,8 +15,17 @@ const gameName = new String('Amandeephc') //string is an object (not array)
 // console.log(gameName.charAt(0));
 // console.log(gameName.indexOf('m'));
 
-const newString = gameName.substring(0, 4)
-console.log(newString);
-const anotherString = gameName.slice(-8,4)
-console.log(anotherString);
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+// const anotherString = gameName.slice(-8,4)
+// console.log(anotherString);
 
+// const newStringOne = "   Amandeep   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+// const url = "https://amandeep.com/amandeep%20singh"
+// console.log(url.replace('%20','-'));
+
+// console.log(url.includes('amandeep'));
+
+console.log(gameName.split('-'));
