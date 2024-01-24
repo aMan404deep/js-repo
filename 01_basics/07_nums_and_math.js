98035000ea6680e5f0cb76aeb8dc8dbda72e3eea
Amandeep Singh <amandeep101003@gmail.com>
2024-01-24 23:17:12 +0530

    Update 07_nums_and_math.js

--- 01_basics/07_nums_and_math.js
+++ 01_basics/07_nums_and_math.js
@@ -22,11 +22,11 @@ const hundreds = 1000000
 // console.log(Math.min(4.2,3,2,1));
 // console.log(Math.max(4.2,3,2,1));
 // console.log(Math.random());
-// console.log((Math.random()*10) + 1);
-
+// console.log((Math.random()*10));
+// console.log((Math.random()) + 1);
 // console.log((Math.random()*10) + 1);
 
 // const min = 10
 // const max = 20
 
-// console.log(Math.floor(Math.random()*(max-min+1)+min));
+//  console.log(Math.floor(Math.random()*(max-min+1)+min));
