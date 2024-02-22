26db9057f1e3791978569564fb7c087a685492be
Amandeep Singh <amandeep101003@gmail.com>
2024-02-22 12:53:07 +0000

    error resolved in colorchanger

--- 07_projects/colorchanger/script.js
+++ 07_projects/colorchanger/script.js
@@ -2,7 +2,7 @@ const buttons  = document.querySelectorAll('.button');
 const body = document.querySelector("body")
 
 buttons.forEach(function(buttons){
-    button.addEventListener('click',function(e){
+    buttons.addEventListener('click',function(e){
         if (e.target.id === 'grey') {
             body.style.backgroundColor = e.target.id;
           }
