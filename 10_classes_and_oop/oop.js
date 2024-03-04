6087233184ba0de7f66ed8d495b5e47460721bd0
Amandeep Singh <amandeep101003@gmail.com>
2024-03-04 11:47:02 +0000

    did some minor changes

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -31,7 +31,7 @@ function User(username, loginCount, isLoggedIn){
     return this
 }
 
-const userOne = new User("hitesh", 12, true)
-const userTwo = new User("ChaiAurCode", 11, false)
+const userOne = new User("amandeep", 12, true)
+const userTwo = new User("singh", 11, false)
 console.log(userOne.constructor);
 //console.log(userTwo);
